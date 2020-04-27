cce36d49f8fd0be4994b8c3b10238a994f6c64c6
Christensen <christensenjj@ad.msoe.edu>
2020-04-27 09:56:58 -0500

    initial commit

---
+++ prob_4.py
@@ -0,0 +1,10 @@
+/**
+ * Project Euler Problem 4
+ * Largest Palindrome Project
+ *
+ * Author: Jacob Christensen <jacobjc2@gmail.com>
+ * Date: 08/31/2019
+ * Desc:
+ * 		Find the largest palindrome made from the 
+ *	    product of two 3-digit numbers.
+**/
